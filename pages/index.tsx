f8f64779ed37195e2cc391992292e60f7c4917a9
ferpa <feririarte.1996@gmail.com>
2023-07-08 12:42:03 -0300

    check if user is connected

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,24 @@
 import { useWeb3Modal } from "@web3modal/react";
 import { Inter } from "next/font/google";
+import { useAccount } from "wagmi";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const { isConnected, address } = useAccount();
   const { open } = useWeb3Modal();
 
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <button onClick={open}>Connect wallet</button>
+      {!isConnected ? (
+        <button onClick={open}>Connect wallet</button>
+      ) : (
+        <>
+          <p>Welcome, ${address}</p>
+        </>
+      )}
     </main>
   );
 }
